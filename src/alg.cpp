86ac4d57a3852c7141f47f47ed70b6ec8f3a1c55
vektor0 <99178713+vektor0@users.noreply.github.com>
2022-06-06 22:33:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,25 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-int z = 0, o = 0, l = size - 1;
-while (o < l) {
-int u = (o + l) / 2;
-if (arr[u] == value) {
-z++;
-int y = u;
-while (arr[--u] == value) {
-z++;
+  int i, d = 0;
+  for (i = 0; i < size; i++)
+      if (arr[i] == value)
+          d++;
+  return d;
+  return 0; // если ничего не найдено
 }
-while (arr[++y] == value) {
-z++;
-}
-return z;
-} else if (arr[u] > value) {
-l = u;
-} else {
-o = u + 1;
-}
-}
-return 0;
-}
-
